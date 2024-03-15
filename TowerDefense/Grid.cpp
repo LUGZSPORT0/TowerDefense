a78d5be3617a72f73a68d32aff0e6d4ce258a9a7
LUGZSPORT0 <arjean85@gmail.com>
2024-03-15 00:01:09 -0400

    started grid class need to continue working on A* pathfinding function

---
+++ TowerDefense/Grid.cpp
@@ -0,0 +1,125 @@
+#include "Grid.h"
+#include "Tile.h"
+#include "Tower.h"
+#include "Enemy.h"
+#include <algorithm>
+
+Grid::Grid(class Game* game)
+	:Actor(game)
+	, mSelectedTile(nullptr)
+{
+	// 7 rows, 16 columns
+	mTiles.resize(NumRows);
+	for (size_t i = 0; i < mTiles.size(); i++)
+	{
+		mTiles[i].resize(NumCols);
+	}
+
+	// Create tiles
+	for (size_t i = 0; i < NumRows; i++)
+	{
+		for (size_t j = 0; j < NumCols; j++)
+		{
+			mTiles[i][j] = new Tile(GetGame());
+			mTiles[i][j]->SetPosition(Vector2(TileSize / 2.0f + j * TileSize, StartY + i * TileSize));
+		}
+	}
+
+	// Set start/end tiles
+	GetStartTile()->SetTileState(Tile::EStart);
+	GetEndTile()->SetTileState(Tile::EBase);
+
+	// Set up adjacency lists
+	for (size_t i = 0; i < NumRows; i++)
+	{
+		for (size_t j = 0; j < NumCols; j++)
+		{
+			if (i > 0)
+			{
+				mTiles[i][j]->mAdjacent.push_back(mTiles[i - 1][j]);
+			}
+			if (i < NumRows - 1)
+			{
+				mTiles[i][j]->mAdjacent.push_back(mTiles[i + 1][j]);
+			}
+			if (j > 0)
+			{
+				mTiles[i][j]->mAdjacent.push_back(mTiles[i][j - 1]);
+			}
+			if (j < NumCols - 1)
+			{
+				mTiles[i][j]->mAdjacent.push_back(mTiles[i][j + 1]);
+			}
+		}
+	}
+
+	// Find Path in reverse
+	FindPath(GetEndTile(), GetStartTile());
+	UpdatePathTiles(GetStartTile());
+
+	mNextEnemy = EnemyTime;
+}
+
+void Grid::SelectTile(size_t row, size_t col)
+{
+	// Make sure it's a valid selection
+	Tile::TileState tstate = mTiles[row][col]->GetTileState();
+	if (tstate != Tile::EStart && tstate != Tile::EBase)
+	{
+		// Deselect previous one
+		if (mSelectedTile)
+		{
+			mSelectedTile->ToggleSelect();
+		}
+		mSelectedTile = mTiles[row][col];
+		mSelectedTile->ToggleSelect();
+	}
+}
+
+void Grid::ProcessClick(int x, int y)
+{
+	y -= static_cast<int>(StartY - TileSize / 2);
+	if (y >= 0)
+	{
+		x /= static_cast<int>(TileSize);
+		y /= static_cast<int>(TileSize);
+		if (x >= 0 && x < static_cast<int>(NumCols) && y >= 0 && y < static_cast<int>(NumRows))
+		{
+			SelectTile(y, x);
+		}
+	}
+}
+
+// Implements A* pathfinding
+bool Grid::FindPath(Tile* start, Tile* goal)
+{
+	for (size_t i = 0; i < NumRows; i++)
+	{
+		for (size_t j = 0; j < NumCols; j++)
+		{
+			mTiles[i][j]->g = 0.0f;
+			mTiles[i][j]->mInOpenSet = false;
+			mTiles[i][j]->mInClosedSet = false;
+		}
+	}
+
+	std::vector<Tile*> openSet;
+
+	// Set current node to start, and add to closed set
+	Tile* current = start;
+	current->mInClosedSet = true;
+
+	do
+	{
+		// Add adjacent nodes to open set
+		for (Tile* neighbor : current->mAdjacent)
+		{
+			if (neighbor->mBlocked)
+			{
+				continue;
+			}
+
+
+		}
+	}
+}
